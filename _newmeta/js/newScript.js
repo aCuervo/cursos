12534774b40847eec6aad0a38b47cf8852d06ba9
Francisco Pérez <fperez@mavint.es>
2017-02-09 18:01:56 +0100

    modPan de manip

---
+++ _newmeta/js/newScript.js
@@ -0,0 +1,9 @@
+﻿function myFunction() {
+    var elmnt = document.getElementById("myDIV");
+    var txt = "";
+    txt += "Height including padding: " + elmnt.clientHeight + "px<br>";
+    txt += "Height including padding and border: " + elmnt.offsetHeight + "px<br>";
+    txt += "Width including padding: " + elmnt.clientWidth + "px<br>";
+    txt += "Width including padding and border: " + elmnt.offsetWidth + "px";
+    document.getElementById("demo").innerHTML = txt;
+}
