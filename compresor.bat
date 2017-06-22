5d2424fcbe9bff29cae81c9469c38c057bc72865
Francisco Pérez <fperez@mavint.es>
2017-06-22 16:15:25 +0200

    minify
    mnt y base y cambios en compresor

--- compresor.bat
+++ compresor.bat
@@ -1,11 +1,12 @@
 @echo off
 set /p curso="Introduce el nombre del curso: "
 set /p modulo="Introduce el nombre del modulo: "
-set /p indice="Introduce el numero del indice: "
+set /p imagen="Introduce el nombre de la carpeta img: "
+set /p indice="Introduce el numero del index: "
 
 REN %~d0%~p0%curso%\index%indice%.html index.html
 COPY %~d0%~p0%curso%\imsmanifest.xml %~d0%~p0
-"C:\Program Files\7-Zip\7z.exe" a -i!%curso%\json\%modulo%.json -i!%curso%\css -i!%curso%\img\comunes -i!%curso%\img\%modulo% -i!%curso%\index.html -i!%curso%\js -i!base.css -i!mnt.js -i!imsmanifest.xml %curso%_%modulo%.zip
+"C:\Program Files\7-Zip\7z.exe" a -i!%curso%\json\%modulo%.json -i!%curso%\css -i!%curso%\img\comunes -i!%curso%\img\%imagen% -i!%curso%\index.html -i!%curso%\js -i!base.css -i!mnt.js -i!imsmanifest.xml %curso%_%modulo%.zip
 REN %~d0%~p0%curso%\index.html index%indice%.html
 DEL %~d0%~p0\imsmanifest.xml
 pause
